187c4d5e01d257daca38fe65df7091098ddb13f6
Patrick510 <117788162+Patrick510@users.noreply.github.com>
2022-11-25 20:11:25 -0400

    Add files via upload

---
+++ Ex02.java
@@ -0,0 +1,15 @@
+/** Escrever o nome de teu curso em uma linha e o nome desta disciplina em outra linha.
+*/
+import java.util.Scanner;
+
+public class Ex02 {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+    System.out.println("Nome: Patrick");
+    System.out.println("Curso: Análise e desenvolvimento de sistemas");
+
+    
+  }
+ }
